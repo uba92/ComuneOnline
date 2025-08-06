19a863bf9e92eba6397752e39c166a668943e282
Umberto Tramontano <u.tramontano92@libero.it>
2025-08-06 23:47:20 +0200

    Added Edit View and HttpPost in CittadiniController

--- Controllers/CittadiniController.cs
+++ Controllers/CittadiniController.cs
@@ -63,5 +63,53 @@ namespace ComuneOnline.Controllers
 
 
         }
+
+        public IActionResult Edit(int id)
+        {
+            if(id<=0)
+            {
+                TempData["ErrorMessage"] = "ID non valido";
+            }
+
+            var cittadino = _context.Cittadini.Find(id);
+            if (cittadino != null)
+            {
+                return View(cittadino);
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Errore! Cittadino non trovato";
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Cittadino cittadino)
+        {
+            if(!ModelState.IsValid)
+            {
+                TempData["ErrorMessage"] = "Errore! Formato dati inseriti non corretti";
+                return View();
+            }
+            var cittadinoToUpdate = await _context.Cittadini.FindAsync(cittadino.CittadinoId);
+            if (cittadinoToUpdate == null)
+            {
+                TempData["ErrorMessage"] = "Errore! Cittadino non trovato";
+                return RedirectToAction(nameof(Index));
+            }
+            // Aggiorno i campi del cittadino
+            cittadinoToUpdate.Nome = cittadino.Nome;
+            cittadinoToUpdate.Cognome = cittadino.Cognome;
+            cittadinoToUpdate.LuogoNascita = cittadino.LuogoNascita;
+            cittadinoToUpdate.IndirizzoResidenza = cittadino.IndirizzoResidenza;
+            cittadinoToUpdate.Email = cittadino.Email;
+            cittadinoToUpdate.Telefono = cittadino.Telefono;
+            
+            await _context.SaveChangesAsync();
+            TempData["SuccessMessage"] = "Cittadino aggiornato con successo!";
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
